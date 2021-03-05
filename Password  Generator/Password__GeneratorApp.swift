08e5488e97d795d3402d6bd9ebaf7dd390964566
DavidGajdics <gajdics.david@hotmail.com>
2021-03-05 20:21:10 +0100

    Initial Commit

---
+++ Password  Generator/Password__GeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Password__GeneratorApp.swift
+//  Password  Generator
+//
+//  Created by David Gajdics on 05.03.21.
+//
+
+import SwiftUI
+
+@main
+struct Password__GeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
